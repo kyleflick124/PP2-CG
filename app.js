49289b39f7386a8dc668a42f1232f4673cf476e6
fernando <fernandof.abromovick@hotmail.com>
2022-09-05 13:55:49 -0300

    triângulo de teste (hello world)

---
+++ app.js
@@ -0,0 +1,125 @@
+//Declarando os shaders em formato de string
+var vertexShaderText = 
+[
+'precision mediump float;',
+'',
+'attribute vec2 vertPosition;',
+'attribute vec3 vertColor;',
+'varying vec3 fragColor;',
+'',
+'void main()',
+'{',
+'  fragColor = vertColor;',
+'  gl_Position = vec4(vertPosition, 0.0, 1.0);',
+'}'
+].join('\n');
+
+var fragmentShaderText =
+[
+'precision mediump float;',
+'',
+'varying vec3 fragColor;',
+'void main()',
+'{',
+'  gl_FragColor = vec4(fragColor, 1.0);',
+'}'
+].join('\n');
+
+//Corpo do projeto, carrega no "onload"
+var InitProject = function (){
+    console.log("funcionou :)");
+
+    var canvas = document.getElementById('main-canvas');
+    var gl = canvas.getContext('webgl');
+
+    if(!gl) {
+        gl = canvas.getContext('experimental-webgl');
+    }
+
+    if(!gl) {
+        alert("Your browser does not support WebGL");
+    }
+
+    //Ajustando o tamanho do canvas para a tela do usuario
+    // canvas.width = window.innerWidth;
+    // canvas.height = window.innerHeight;
+    // gl.viewport(0, 0, window.innerWidth, window.innerHeight);
+
+    //limpando o contexto do webgl
+    gl.clearColor(0.75, 0.85, 0.8, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    //Criando shaders
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+    gl.shaderSource(vertexShader, vertexShaderText);
+    gl.shaderSource(fragmentShader, fragmentShaderText);
+
+    gl.compileShader(vertexShader);
+    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)){
+        console.error("Error compiling vertex shader", gl.getShaderInfoLog(vertexShader));
+        return;
+    }
+
+    gl.compileShader(fragmentShader);
+	if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+		console.error('Error compiling fragment shader', gl.getShaderInfoLog(fragmentShader));
+		return;
+	}
+
+    //Criando o programa com os shaders criados
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+	if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+		console.error('ERROR linking program!', gl.getProgramInfoLog(program));
+		return;
+	}
+	gl.validateProgram(program);
+	if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
+		console.error('ERROR validating program!', gl.getProgramInfoLog(program));
+		return;
+	}
+
+    //Criando buffer dos vertices
+    var triangleVertices =
+    [ // X, Y,       R, G, B
+		0.0, 0.5,    1.0, 1.0, 0.0,
+		-0.5, -0.5,  0.7, 0.0, 1.0,
+		0.5, -0.5,   0.1, 1.0, 0.6
+	];
+
+    var triangleVertexBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
+
+    var positionAttribLocation = gl.getAttribLocation(program, "vertPosition");
+    var colorAttribLocation = gl.getAttribLocation(program, 'vertColor');
+    gl.vertexAttribPointer(
+        positionAttribLocation, //Attribute location
+        2, //Number of elements per attribute
+        gl.FLOAT, //type of elements
+        gl.FALSE, //normalization
+        5 * Float32Array.BYTES_PER_ELEMENT, //Size of an individual vertex
+        0//Offset from the beginning of a single vertex to this attribute
+    );
+
+    gl.vertexAttribPointer(
+        colorAttribLocation, //Attribute location
+        3, //Number of elements per attribute
+        gl.FLOAT, //type of elements
+        gl.FALSE, //normalization
+        5 * Float32Array.BYTES_PER_ELEMENT, //Size of an individual vertex
+        2 * Float32Array.BYTES_PER_ELEMENT//Offset from the beginning of a single vertex to this attribute
+    );
+
+    gl.enableVertexAttribArray(positionAttribLocation);
+    gl.enableVertexAttribArray(colorAttribLocation);
+
+    //Loop de renderizacao (atualizacao da tela)
+    gl.useProgram(program);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+}
